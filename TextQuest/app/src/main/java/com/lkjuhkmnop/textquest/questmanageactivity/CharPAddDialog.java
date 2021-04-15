a0de8d4383927a377867fd87c70baf20aa7041f8
lkjuhkmnop <maksimkide@gmail.com>
2021-04-15 15:08:36 +0300

    QuestManageActivity design, univesal adapter, parameters RecyclerView

--- TextQuest/app/src/main/java/com/lkjuhkmnop/textquest/questmanageactivity/CharPAddDialog.java
+++ TextQuest/app/src/main/java/com/lkjuhkmnop/textquest/questmanageactivity/CharPAddDialog.java
@@ -15,17 +15,23 @@ import androidx.fragment.app.DialogFragment;
 
 import com.lkjuhkmnop.textquest.R;
 
-public class CharPropDialog extends DialogFragment {
+public class CharPAddDialog extends DialogFragment {
+    private int charPDataType;
+
+    public CharPAddDialog(int charPDataType) {
+        this.charPDataType = charPDataType;
+    }
+
     /* The activity that creates an instance of this dialog fragment must
      * implement this interface in order to receive event callbacks.
      * Each method passes the DialogFragment in case the host needs to query it. */
-    public interface CharPropDialogListener {
-        public void onCharPropDialogPositiveClick(String charPropAddName);
-        public void onCharPropDialogNegativeClick();
+    public interface CharPDataAddDialogListener {
+        public void onCharPDataAddDialogPositiveClick(int charPDataType, String charPDataAddName);
+        public void onCharPDataAddDialogNegativeClick();
     }
 
     // Use this instance of the interface to deliver action events
-    CharPropDialogListener listener;
+    CharPDataAddDialogListener listener;
 
     // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
     @Override
@@ -34,7 +40,7 @@ public class CharPropDialog extends DialogFragment {
         // Verify that the host activity implements the callback interface
         try {
             // Instantiate the NoticeDialogListener so we can send events to the host
-            listener = (CharPropDialogListener) context;
+            listener = (CharPDataAddDialogListener) context;
         } catch (ClassCastException e) {
             // The activity doesn't implement the interface, throw exception
             throw new ClassCastException(getActivity().toString() + " must implement NoticeDialogListener");
@@ -47,21 +53,21 @@ public class CharPropDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.char_prop_add_dialog, null);
+        View dialogView = inflater.inflate(R.layout.char_p_data_add_dialog, null);
 
-        builder.setTitle(R.string.char_prop_add_dialog_title);
+        builder.setTitle(R.string.char_p_data_add_dialog_title);
         builder.setView(dialogView);
-        builder.setPositiveButton(R.string.char_prop_add_ok, (dialog, which) -> {
-            EditText charPropAddNameET = (EditText) dialogView.findViewById(R.id.char_prop_add_name);
-            listener.onCharPropDialogPositiveClick(charPropAddNameET.getText().toString());
+        builder.setPositiveButton(R.string.char_p_data_add_ok, (dialog, which) -> {
+            EditText charPropAddNameET = (EditText) dialogView.findViewById(R.id.char_p_data_add_name);
+            listener.onCharPDataAddDialogPositiveClick(charPDataType, charPropAddNameET.getText().toString());
         })
-                .setNegativeButton(R.string.char_prop_add_cancel, (dialog, which) -> listener.onCharPropDialogNegativeClick());
+                .setNegativeButton(R.string.char_p_data_add_cancel, (dialog, which) -> listener.onCharPDataAddDialogNegativeClick());
 
         AlertDialog dialog = builder.create();
         dialog.setCanceledOnTouchOutside(true);
         dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
-        dialogView.findViewById(R.id.char_prop_add_name).requestFocus();
+        dialogView.findViewById(R.id.char_p_data_add_name).requestFocus();
 
         return dialog;
     }
--- TextQuest/app/src/main/java/com/lkjuhkmnop/textquest/questmanageactivity/QuestManageActivity.java
+++ TextQuest/app/src/main/java/com/lkjuhkmnop/textquest/questmanageactivity/QuestManageActivity.java
@@ -1,6 +1,5 @@
 package com.lkjuhkmnop.textquest.questmanageactivity;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,8 +8,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,28 +20,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.Objects;
 
-public class QuestManageActivity extends AppCompatActivity implements CharPropDialog.CharPropDialogListener {
+public class QuestManageActivity extends AppCompatActivity implements CharPAddDialog.CharPDataAddDialogListener {
+    public static final int CHAR_PARAMETER = 0;
+    public static final int CHAR_PROPERTY = 1;
     private int action;
 
     private EditText questTitle, questAuthor;
-    private Button questJsonFile;
-    private ImageView questAddCharProperty, questAddCharParameter;
+    private Button questJsonFile, questManageOk;
+    private ImageView[] questAddCharPDataButtons;
     private RecyclerView questCharPropertiesRecView, questCharParametersRecView;
-    private CharPropertiesAdapter charPropertiesAdapter;
+    private CharPDataAdapter[] charPDataAdapters;
 //    private TextView tw;
 
     private static final int PICK_JSON_FILE_CODE = 1;
 
-    private static final String ADD_CHAR_PROP_DIALOG_TAG = "ADD_CHAR_PROP_DIALOG_TAG";
+    private static final String ADD_CHAR_P_DATA_ITEM_DIALOG_TAG = "ADD_CHAR_P_DATA_ITEM_DIALOG_TAG";
 
     private String questJson;
     private LinkedHashMap<String, String> questCharacterProperties = new LinkedHashMap<>();
-    private LinkedList<String[]> questCharacterParameters = new LinkedList<>();
+    private LinkedHashMap<String, String> questCharacterParameters = new LinkedHashMap<>();
+    private LinkedHashMap<String, String>[] questCharPData = new LinkedHashMap[2];
+
+    {
+        questCharPData[CHAR_PROPERTY] = questCharacterProperties;
+        questCharPData[CHAR_PARAMETER] = questCharacterParameters;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,31 +55,36 @@ public class QuestManageActivity extends AppCompatActivity implements CharPropDi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest_manage);
 
+        charPDataAdapters = new CharPDataAdapter[2];
+        questAddCharPDataButtons = new ImageView[2];
+
         questTitle = (EditText) findViewById(R.id.quest_title);
         questAuthor = (EditText) findViewById(R.id.quest_author);
         questJsonFile = (Button) findViewById(R.id.quest_json_file_button);
-        questAddCharProperty = (ImageView) findViewById(R.id.quest_add_character_property);
-        questAddCharParameter = (ImageView) findViewById(R.id.quest_add_character_parameter);
+        questAddCharPDataButtons[CHAR_PROPERTY] = (ImageView) findViewById(R.id.quest_add_character_property);
+        questAddCharPDataButtons[CHAR_PARAMETER] = (ImageView) findViewById(R.id.quest_add_character_parameter);
         questCharPropertiesRecView = (RecyclerView) findViewById(R.id.quest_character_properties_recycler_view);
         questCharParametersRecView = (RecyclerView) findViewById(R.id.quest_character_parameters_recycler_view);
+        questManageOk = (Button) findViewById(R.id.quest_manage_ok_button);
 //        tw = findViewById(R.id.textView);
 
+
+
 //        Set layout manager and adapter for character properties RecycleView
-        charPropertiesAdapter = new CharPropertiesAdapter(this, questCharacterProperties);
-        questCharPropertiesRecView.setLayoutManager(new LinearLayoutManager(this) {
-            @Override
-            public void onItemsChanged(@NonNull RecyclerView recyclerView) {
-                super.onItemsChanged(recyclerView);
-                recyclerView.swapAdapter(charPropertiesAdapter, false);
-            }
-        });
-        questCharPropertiesRecView.setAdapter(charPropertiesAdapter);
+        charPDataAdapters[CHAR_PROPERTY] = new CharPDataAdapter(this, questCharacterProperties);
+        questCharPropertiesRecView.setLayoutManager(new LinearLayoutManager(this));
+        questCharPropertiesRecView.setAdapter(charPDataAdapters[CHAR_PROPERTY]);
+
+//        Set layout manager and adapter for character parameters RecycleView
+        charPDataAdapters[CHAR_PARAMETER] = new CharPDataAdapter(this, questCharacterParameters);
+        questCharParametersRecView.setLayoutManager(new LinearLayoutManager(this));
+        questCharParametersRecView.setAdapter(charPDataAdapters[CHAR_PARAMETER]);
 
 //        Action type (add new quest / manage existing quest settings)
 //        Received as extra in intent
         action = getIntent().getIntExtra(Tools.QUEST_MANAGE_ACTION_EXTRA_NAME, Tools.QUEST_MANAGE_ACTION_ADD_QUEST);
 
-//        Choose json file button
+//        Click listener for json file button
         questJsonFile.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -84,11 +92,18 @@ public class QuestManageActivity extends AppCompatActivity implements CharPropDi
             startActivityForResult(intent, PICK_JSON_FILE_CODE);
         });
 
-//        User clicked on add character property
-//        Show a dialog to get prop. name
-        questAddCharProperty.setOnClickListener(v -> {
-            CharPropDialog charPropDialog = new CharPropDialog();
-            charPropDialog.show(getSupportFragmentManager(), ADD_CHAR_PROP_DIALOG_TAG);
+//        Click listener for add character property button
+        questAddCharPDataButtons[CHAR_PROPERTY].setOnClickListener(v -> {
+//          Show a dialog to get property name
+            CharPAddDialog charPropDialog = new CharPAddDialog(CHAR_PROPERTY);
+            charPropDialog.show(getSupportFragmentManager(), ADD_CHAR_P_DATA_ITEM_DIALOG_TAG);
+        });
+
+//        Click listener for add character parameter button
+        questAddCharPDataButtons[CHAR_PARAMETER].setOnClickListener(v -> {
+//          Show a dialog to get property name
+            CharPAddDialog charParamDialog = new CharPAddDialog(CHAR_PARAMETER);
+            charParamDialog.show(getSupportFragmentManager(), ADD_CHAR_P_DATA_ITEM_DIALOG_TAG);
         });
     }
 
@@ -113,8 +128,8 @@ public class QuestManageActivity extends AppCompatActivity implements CharPropDi
         }
     }
 
-    public void reloadCharPropsRecView() {
-        charPropertiesAdapter.notifyDataSetChanged();
+    public void notifyCharPDataRecViewAdapter(int charPDataType) {
+        charPDataAdapters[charPDataType].notifyDataSetChanged();
 //        questCharPropertiesRecView.
 //        questCharPropertiesRecView.requestLayout();
 //        questCharPropertiesRecView.
@@ -123,13 +138,14 @@ public class QuestManageActivity extends AppCompatActivity implements CharPropDi
 
 //    Add character property with specified name
     @Override
-    public void onCharPropDialogPositiveClick(String charPropAddName) {
-        if (questCharacterProperties.containsKey(charPropAddName)) {
-            Toast.makeText(this, getString(R.string.char_prop_add_have_such_name), Toast.LENGTH_LONG).show();
+    public void onCharPDataAddDialogPositiveClick(int charPDataType, String charPropAddName) {
+        if (questCharacterProperties.containsKey(charPropAddName) || questCharacterParameters.containsKey(charPropAddName)) {
+            Toast.makeText(this, getString(R.string.char_p_data_add_name_used), Toast.LENGTH_LONG).show();
         } else {
-            questCharacterProperties.put(charPropAddName, "");
-            reloadCharPropsRecView();
+            questCharPData[charPDataType].put(charPropAddName, "");
+            notifyCharPDataRecViewAdapter(charPDataType);
         }
+        questAddCharPDataButtons[charPDataType].requestFocus();
 //        RecyclerView.ViewHolder addedItemViewHolder = questCharPropertiesRecView.findViewHolderForAdapterPosition(0);
 //        Log.d("dialog", "end");
 //        addedItemViewHolder.requestValueFocus();
@@ -137,8 +153,8 @@ public class QuestManageActivity extends AppCompatActivity implements CharPropDi
 
 //    User cancel adding a new char. prop.
     @Override
-    public void onCharPropDialogNegativeClick() {
-        Toast.makeText(this, getText(R.string.char_prop_add_cancel_msg), Toast.LENGTH_SHORT).show();
+    public void onCharPDataAddDialogNegativeClick() {
+        Toast.makeText(this, getText(R.string.char_p_data_add_cancel_msg), Toast.LENGTH_SHORT).show();
 
     }
 }
